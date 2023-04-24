7a94aee814ee7194a81f4970b0b4fc7e15f0042c
Steve N. Gachanja [Digital Banking] <sngachanja@CO-OPBANK.CO.KE>
2023-04-24 11:46:39 +0300

    simple python application

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def hello_world():
-    return 'hello, Simple Flask Application'
+def hello_geek():
+    return '<div style=\"text-align: center;\"><h1>Hello from KodeKloud!</h1><p>Welcome to GCP DevOps Course.</p><p></p></div>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
